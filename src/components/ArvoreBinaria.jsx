4031a9b60efcd2c72dfc8f4475e19faf9028721a
Vanessa Trajano <vanessapalmeirakelmertrajano@gmail.com>
2025-02-04 18:50:12 -0300

    Cria estrutura base do componente ArvoreBinaria

--- src/components/ArvoreBinaria.jsx
+++ src/components/ArvoreBinaria.jsx
@@ -1,11 +1,19 @@
-import './ArvoreBinaria.css'
+import "./ArvoreBinaria.css";
 
 function ArvoreBinaria() {
   return (
-    <>
-      Funciona!
-    </>
-  )
+    <div>
+      <h1>Árvore Binária</h1>
+      <div>
+        <input type="number" placeholder="valor" />
+        <button>Inserir</button>
+        <button>Remover</button>
+      </div>
+      <div>
+        <p> estrutura da árvore vai entrar aqui </p>
+      </div>
+    </div>
+  );
 }
 
-export default ArvoreBinaria
+export default ArvoreBinaria;
