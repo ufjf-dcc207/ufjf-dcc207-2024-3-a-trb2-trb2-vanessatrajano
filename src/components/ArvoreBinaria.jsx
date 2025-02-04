8ddd20f654eba40244ef468fd8ffdb072e4c88c0
Vanessa Trajano <vanessapalmeirakelmertrajano@gmail.com>
2025-02-04 19:28:17 -0300

    Cria estados

--- src/components/ArvoreBinaria.jsx
+++ src/components/ArvoreBinaria.jsx
@@ -1,12 +1,20 @@
+import { useState } from "react";
 import "./ArvoreBinaria.css";
 import No from "./No";
 
 function ArvoreBinaria() {
+  const [arvore, setArvore] = useState({
+    valor: 10,
+    esquerda: null,
+    direita: null,
+  });
+  const [valor, setValor] = useState();
+
   return (
     <div>
       <h1>Árvore Binária</h1>
       <div>
-        <input type="number" placeholder="valor" />
+        <input type="number" placeholder="valor" value={valor} />
         <button>Inserir</button>
         <button>Remover</button>
       </div>
