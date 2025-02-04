f6da42eefd770fbd376d215aa575cb164c8d638d
Vanessa Trajano <vanessapalmeirakelmertrajano@gmail.com>
2025-02-04 19:04:14 -0300

    Cria estrutura básica para o componente do Nó

--- src/components/No.jsx
+++ src/components/No.jsx
@@ -1,11 +1,19 @@
-import './No.css'
+import "./No.css";
 
 function No() {
   return (
-    <>
-      Funciona!
-    </>
-  )
+    <div>
+      <span>5</span>
+      <div>
+        <div>
+          <span>Esquerda: 3</span>
+        </div>
+        <div>
+          <span>Direita: 10</span>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default No
+export default No;
