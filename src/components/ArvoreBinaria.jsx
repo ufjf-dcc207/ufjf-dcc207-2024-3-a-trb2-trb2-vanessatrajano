c2a31653f10507886db972fb07bd72862147ccd2
Vanessa Trajano <vanessapalmeirakelmertrajano@gmail.com>
2025-02-04 20:21:13 -0300

    Adiciona lógica de inserção

--- src/components/ArvoreBinaria.jsx
+++ src/components/ArvoreBinaria.jsx
@@ -2,13 +2,30 @@ import { useState } from "react";
 import "./ArvoreBinaria.css";
 import No from "./No";
 
+function inserirNo(no, valor) {
+  if (no === null) {
+    return { valor, esquerda: null, direita: null };
+  }
+  if (valor < no.valor) {
+    return { ...no, esquerda: inserirNo(no.esquerda, valor) };
+  } else if (valor > no.valor) {
+    return { ...no, direita: inserirNo(no.direita, valor) };
+  }
+  return no;
+}
+
 function ArvoreBinaria() {
   const [arvore, setArvore] = useState({
-    valor: 10,
+    valor: 20,
     esquerda: { valor: 5, esquerda: null, direita: null },
-    direita: { valor: 15, esquerda: null, direita: null },
+    direita: { valor: 30, esquerda: null, direita: null },
   });
   const [valor, setValor] = useState();
+
+  const manipularInsercao = () => {
+    setArvore(inserirNo(arvore, valor));
+    setValor("");
+  };
 
   return (
     <div>
@@ -20,7 +37,7 @@ function ArvoreBinaria() {
           value={valor}
           onChange={(e) => setValor(e.target.value)}
         />
-        <button>Inserir</button>
+        <button onClick={manipularInsercao}>Inserir</button>
         <button>Remover</button>
       </div>
       <div>
