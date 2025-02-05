ead949c2f1c7ed753e00faa95153a181cb50d0cc
Vanessa Trajano <vanessapalmeirakelmertrajano@gmail.com>
2025-02-05 11:30:01 -0300

    Adiciona base no vite.config

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/ufjf-dcc207-2024-3-a-trb2-trb2-vanessatrajano",
   plugins: [react()],
 })
